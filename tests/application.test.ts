d24bddfeef020b989715226cd42b5e2cf031e64d
Arvid Petermann <arvidpetermann@googlemail.com>
2024-10-18 14:12:46 +0100

    chores(lint): fix console.log

--- tests/application.test.ts
+++ tests/application.test.ts
@@ -6,7 +6,6 @@ describe('node-argocd/application', () => {
   it('list', async () => {
     const acdc = new Client();
     const test = await acdc.application.list();
-    console.log('test', test.data?.items);
     expect(test).to.be.true;
   });
 });
