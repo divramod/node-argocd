ac5469ae122c54ffec3e9e5da1542a1e6a193aa3
Arvid Petermann <arvidpetermann@googlemail.com>
2024-10-18 18:21:05 +0100

    feat(client): fix wrong token source

--- publish.sh
+++ publish.sh
@@ -3,5 +3,7 @@ AREA=${1}
 FEATURE=${2}
 jq '.version |= (. | split(".") | .[2] |= tostring | .[0] + "." + .[1] + "." + (.[2] | tonumber + 1 | tostring))' package.json > tmp.$$.json && mv tmp.$$.json package.json
 git pu "feat(${1}): ${2}" || true
+export NPM_TOKEN=$($HOME/.config/mo/plug/npm/token-divramod)
+echo "NPM_TOKEN: ${NPM_TOKEN}"
 pnpm build
 pnpm publish --no-git-checks
