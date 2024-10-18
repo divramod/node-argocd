f737af1b5dba23d432a6cc4cd4c9729b0723da11
Arvid Petermann <arvidpetermann@googlemail.com>
2024-10-18 19:06:15 +0100

    feat(publish): add NPM_AUTH_TOKEN

--- publish.sh
+++ publish.sh
@@ -6,6 +6,7 @@ git pu "feat(${1}): ${2}" || true
 pnpm build
 # NPM_PASSWORD=$(cat $HOME/.config/mo/plugs/npm/password-divramod)
 NPM_TOKEN=$(cat $HOME/.config/mo/plugs/npm/token-divramod)
+npm config set //registry.npmjs.org/:_authToken=${NPM_TOKEN}
 # echo "NPM_TOKEN: ${NPM_TOKEN}"
 # npm-cli-login -u divramod-dev -e divramod@gmail.com -p ${NPM_PASSWORD}
 pnpm publish
