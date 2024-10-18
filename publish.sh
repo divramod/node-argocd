a12b48074cc15e5a1e563d70dfc37113ba8cb25e
Arvid Petermann <arvidpetermann@googlemail.com>
2024-10-18 18:30:57 +0100

    feat(publish): add NPM_AUTH_TOKEN

--- publish.sh
+++ publish.sh
@@ -4,6 +4,6 @@ FEATURE=${2}
 jq '.version |= (. | split(".") | .[2] |= tostring | .[0] + "." + .[1] + "." + (.[2] | tonumber + 1 | tostring))' package.json > tmp.$$.json && mv tmp.$$.json package.json
 git pu "feat(${1}): ${2}" || true
 pnpm build
-export NPM_AUTH_TOKEN=$(cat $HOME/.config/mo/plugs/npm/token-divramod)
-echo "NPM_AUTH_TOKEN: ${NPM_AUTH_TOKEN}"
-pnpm publish --no-git-checks
+export NPM_TOKEN=$(cat $HOME/.config/mo/plugs/npm/token-divramod)
+echo "NPM_TOKEN: ${NPM_TOKEN}"
+pnpm publish
